a7675e7bb003de213342a1c495f1f30c06131468
João Pedro Alves Pinto <20251042060027@ifrn.local>
2025-12-10 13:47:24 -0300

    Added testing structure

---
+++ tests/test.py
@@ -0,0 +1,13 @@
+import unittest
+from pathlib import Path
+
+import main
+
+TEST_DATA_PATH = Path('')
+
+class TestErrorChecking(unittest.TestCase):
+	def test_check_for_errors(self):
+		self.assertEqual()
+
+if __name__ == "__main__":
+	unittest.main()
